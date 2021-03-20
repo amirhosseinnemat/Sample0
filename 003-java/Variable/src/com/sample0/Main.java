98815f54089d402795537cec4d42ae7b40654d0d
amir <ahosseinnemat@gmail.com>
2021-03-20 19:15:40 +0330

    new update

---
+++ 003-java/Variable/src/com/sample0/Main.java
@@ -0,0 +1,19 @@
+package com.sample0;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String name = "Amir";
+        String family = "Hosseinnemat";
+        System.out.println("I'm " + name + " " + family);
+
+        int myAge = 26;
+        System.out.println("My age is : " + myAge);
+
+        Date now = new Date();
+        System.out.println("today is : " + now);
+    
+    }
+}
