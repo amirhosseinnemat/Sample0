637ecd319591063f568516ccbffa0bc8c0364ba5
Amir <ahosseinnemat@gmail.com>
2021-03-26 09:44:40 +0430

    new update

---
+++ 003-java/0006-for-each/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] fruits = { "banana", "pineapple", "apple" };
+        for (String mive : fruits) {
+            System.out.println(mive);
+
+        }
+    }
+}
