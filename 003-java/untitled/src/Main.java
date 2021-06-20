13dc3212252b18192ab032c46d534aa19f49a7c4
Amir <ahosseinnemat@gmail.com>
2021-06-20 23:16:53 +0430

    update

---
+++ 003-java/untitled/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b =1;
+        for (int i = 0 ; i < 10 ; i++)
+        {
+            a = a+b;
+            b = b-a;
+        }
+        System.out.println(a);
+    }
+}
