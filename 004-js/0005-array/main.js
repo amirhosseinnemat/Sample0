4ad3f8efb124b412b458f27cb109a3be229922f4
Amir <ahosseinnemat@gmail.com>
2021-06-21 16:00:37 +0430

    add array and object sample

---
+++ 004-js/0005-array/main.js
@@ -0,0 +1,23 @@
+var arr1 = ["ali", "farahani", 25];
+
+/* --- array method ---
+
+// add to last
+arr1.push("amir");
+
+// add to first
+arr1.unshift("mohsen");
+
+// delete from last
+arr1.pop();
+
+// delete from first
+arr1.shift();
+
+// show index
+var wordIndex = arr1.indexOf("farahani");
+console.log(wordIndex);
+
+ */
+
+console.log(arr1);
---
+++ 004-js/0006-object/main.js
@@ -0,0 +1,11 @@
+var profile = {
+    name: "Amir",
+    family: "Moghadam",
+    age: 29
+};
+
+console.log(profile);
+
+profile.name = "Ali";
+
+console.log(profile);
