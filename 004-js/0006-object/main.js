41e8f47bfd72878cba7abe83ab16cad5f36b49e4
Amir <ahosseinnemat@gmail.com>
2021-06-21 16:14:59 +0430

    update object sample

--- 004-js/0006-object/main.js
+++ 004-js/0006-object/main.js
@@ -1,7 +1,14 @@
 var profile = {
     name: "Amir",
     family: "Moghadam",
-    age: 29
+    age: 29,
+    player: ["Ali", "Jafar", "Saeed"],
+    exam: function (score) {
+        if (score >= 10)
+            alert("Congrats! you pass the exam.");
+        else
+            alert("Oops! you failed, you should study harder.")
+    }
 };
 
 console.log(profile);
@@ -9,3 +16,5 @@ console.log(profile);
 profile.name = "Ali";
 
 console.log(profile);
+
+profile.exam(8);
